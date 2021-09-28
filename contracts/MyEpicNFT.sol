5a5daadb4a8245c1b3083914138a2d5a7a871015
imanuvi <inv484@gmail.com>
2021-09-28 22:57:11 +0700

    save image as svg on chain

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -32,7 +32,7 @@ contract MyEpicNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // Set the first NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/2BKT");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiQU9UIiwKICAgICJkZXNjcmlwdGlvbiI6ICJORlQgdGhhdCBkaXNwbGF5IG1vdmllIHRpdGxlIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStVMmhwYm1kbGEya2dUbThnUzNsdmFtbHVQQzkwWlhoMFBnbzhMM04yWno0PSIKfQ==");
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
         
