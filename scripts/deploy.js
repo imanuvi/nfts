0281a23bd1de0166c43ba1e7f13c467561766b91
imanuvi <inv484@gmail.com>
2021-09-28 17:42:32 +0700

    setup rinkeby network, mint nft on rinkeby

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+const main = async () => {
+    // Compile our file and generate the necessary file under `artifacts` directory.
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    
+    // Hardhat will create LOCAL ethereum network, but just for THIS contract.
+    // After script completes it will DESTROY that local ethereum network.
+    const nftContract = await nftContractFactory.deploy();
+    
+    // Wait until the contract is officially mined and deployed to our blockchain.
+    // Hardhat actually create fakers `miners` on your machine to imitate the actual blockchain.
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+
+    // Mint another nft for fun.
+    txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+}
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
